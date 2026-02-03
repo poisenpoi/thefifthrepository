4c1adf2b5b8a7c81a3e368114236c59160f14f2d
Poi <fgmex95@gmail.com>
2026-02-03 21:01:38 +0700

    ss

--- components/CourseDetail.tsx
+++ components/CourseDetail.tsx
@@ -181,7 +181,7 @@ export default async function CourseDetails({
             </div>
           </div>
 
-          <div className="lg:col-span-1 lg:sticky lg:top-13 z-20">
+          <div className="lg:col-span-1 lg:sticky lg:top-13 lg:mt-9 z-20">
             <div className="bg-white rounded-2xl border border-slate-200/60 shadow-2xl shadow-slate-900/5 overflow-hidden">
               <div className="aspect-video relative bg-slate-100 border-b border-slate-200/60">
                 <img
